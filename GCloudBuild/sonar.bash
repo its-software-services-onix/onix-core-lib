841a0993e4053227e61343d88c5e999bbb696f08
pjamenaja <pjame.fb@gmail.com>
2019-09-13 22:06:24 +0700

    Added CICD

---
+++ GCloudBuild/sonar.bash
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#Be careful to set this file without \r
+
+WORK_DIR=/OnixCore
+SOLUTION_DIR=$WORK_DIR/OnixCore
+
+mkdir -p ${WORK_DIR}
+cd ${WORK_DIR}
+git clone https://github.com/pjamenaja/onix-core.git
+cd ${SOLUTION_DIR}
+git checkout ${COMMIT_SHA} -b ${COMMIT_SHA}
+
+dotnet sonarscanner begin \
+    /key:pjamenaja_onix-core \
+    /o:pjamenaja \
+    /v:${BUILT_VERSION} \
+    /d:sonar.host.url=https://sonarcloud.io \
+    /d:sonar.branch.name=${BRANCH_NAME} \
+    /d:sonar.cs.opencover.reportsPaths=./coverage.opencover.xml \
+    /d:sonar.javascript.exclusions=**/bootstrap/**,**/jquery/**,**/jquery-validation/**,**/jquery-validation-unobtrusive/** \
+    /d:sonar.verbose=true \
+    /d:sonar.scm.provider=git \
+    /d:sonar.login=${SONAR_KEY}
+
+dotnet build OnixCore.sln
+
+coverlet './OnixCoreTest/bin/Debug/netcoreapp2.2/OnixCoreTest.dll' --target 'dotnet' --targetargs 'test . --no-build' --format opencover
+if [ "$?" -ne "0" ]; then
+  echo "Exit code from coverlet is not zero!!!"
+  exit 1
+fi
+
+dotnet sonarscanner end /d:sonar.login=${SONAR_KEY}
