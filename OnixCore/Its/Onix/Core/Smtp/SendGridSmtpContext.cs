179116b406014e4c3187ffd02c36a27ba44175ae
pjamenaja <pjame.fb@gmail.com>
2019-11-25 21:48:55 +0700

    Added SendGrid

--- OnixCore/Its/Onix/Core/Smtp/SendGridSmtpContext.cs
+++ OnixCore/Its/Onix/Core/Smtp/SendGridSmtpContext.cs
@@ -1,9 +1,22 @@
+using SendGrid;
+using SendGrid.Helpers.Mail;
+
 namespace Its.Onix.Core.Smtp
 {
 	public class SendGridSmtpContext : SmtpContextBase
 	{
         public SendGridSmtpContext() : base()
         {
+        }
+
+        public override void Send(Mail mail)
+        {
+            string apiKey = GetSmtpPassword();
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress(mail.From, mail.FromName);
+            var to = new EmailAddress(mail.To);
+            var msg = MailHelper.CreateSingleEmail(from, to, mail.Subject, mail.Body, mail.Body);
+            client.SendEmailAsync(msg);
         }        
     }    
 }
